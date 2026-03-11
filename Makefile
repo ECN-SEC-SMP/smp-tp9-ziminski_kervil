e0fe7135b373b540a3274183e5d7bfcb350ff18b
Zaryus <contact.zaryus@gmail.com>
2026-03-11 16:41:10 +0100

    Boucle de l'exo 1

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all: exercice1
 
 # Commande de compilation directe
 exercice1: exercice1.cpp
-	g++ -Wall -Wextra -std=c++17 exercice1.cpp -o exercice1
+	g++ -Wall -Wextra -std=c++17 exercice1.cpp -o exercice1.out
 
 # Règle pour supprimer l'exécutable
 clean:
--- exercice1.cpp
+++ exercice1.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -10,11 +11,34 @@ int main(void){
 
 
     //On affecte des valeurs 3, 1, 4
-    v.push_back(3);
-    v.push_back(1);
-    v.push_back(4);
+    v[0] = 3; 
+    v[1] = 1; 
+    v[2] = 4; 
 
+    /* --- Parcours des valeurs --- */
+
+    cout << "Parcours par Indice :" << endl; 
+
+    // 1. Parcours par indice
+
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << endl;  
+    }
+
+    cout << "Range based loop : " << endl; 
     
+    // Range based loop
+    for(int i : v){
+        cout << i << endl;  
+    }
+
+    cout << "A l'aide d'un intérateur : " << endl; 
+    
+    vector<int>::iterator it; 
+    for(it = v.begin(); it != v.end(); it++){
+        cout << *it << endl;
+    }
+
 
     return 0; 
 }
